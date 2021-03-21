8b9900ea38fed291e841e8ee8e52a7754af1ba9f
miguel angel <miguelequilea18@gmail.com>
2021-03-21 11:11:48 -0300

    update java 8 to java 11

---
+++ src/main/java/com/springboot/moviemoon/MoviemoonApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.moviemoon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoviemoonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoviemoonApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springboot/moviemoon/MoviemoonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.moviemoon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoviemoonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
